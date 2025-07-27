43604a35156dd02c2d344d131f11ebed359ba637
Aliyan Ali <darkpikayt1@gmail.com>
2025-07-27 09:41:16 +0500

    arcjet middleware

---
+++ middleware/arcjet.middleware.js
@@ -0,0 +1,22 @@
+import aj from "../config/arcjet.js";
+
+const arcjetMiddleware=async(req,res,next)=>{
+    try {
+        const desicion=await aj.protect(req, {requested:1});
+
+      if(desicion.isDenied()){
+        if(desicion.reason.isRateLimit()) return res.status(429).json({message: "to many requests"})
+    
+        if(desicion.reason.isBot()) return res.status(403).json({message: "bot detected"})
+
+        return res.status(403).json({message: "Acess denied"})
+    }
+    next();
+    } catch (error) {
+        console.log("error in arcjetMiddleware")
+         res.status(403).json({message: error.message})
+    }
+}
+
+export default arcjetMiddleware;
+
