32582f8fbd05f2013253e57ac43bc4d84351cc50
Aliyan Ali <darkpikayt1@gmail.com>
2025-07-29 09:46:01 +0500

    upstash.js

--- config/upstash.js
+++ config/upstash.js
@@ -1,7 +1,7 @@
-import { Client as Workflow } from "@upstash/qstash";
-import { QSTASH_TOKEN, QSTASH_URL } from "./envConfig";
+import { Client as Workflow } from "@upstash/workflow";
+import { QSTASH_TOKEN, QSTASH_URL } from "./envConfig.js";
 
-export default workFlowClient=new Workflow({
+export const workFlowClient=new Workflow({
     baseUrl: QSTASH_URL,
     token: QSTASH_TOKEN
 });
